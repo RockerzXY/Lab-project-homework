4357580546f4f1a4ba9695bd44611e4aed4133cd
RockerzXY <117651685+RockerzXY@users.noreply.github.com>
2023-09-29 12:52:07 +0300

    Добавление лаб 9.

--- lab9_4.py
+++ lab9_4.py
@@ -1,3 +1,17 @@
 # Лаб 9. Задание 4.
 from pathlib import Path
 
+
+with open(Path('files/show_lldp_neighbors_detail.txt'), 'r') as f:
+    data = f.read()
+
+sys_name, port_id = '', ''
+for elem in data.split('\n'):
+    elem_list = elem.split(': ')
+
+    if elem_list[0] == 'System Name':
+        sys_name = elem_list[1]
+    elif elem_list[0] == 'Port id':
+        port_id = elem_list[1]
+
+print(f'System Name: {sys_name} | Port ID: {port_id}')
